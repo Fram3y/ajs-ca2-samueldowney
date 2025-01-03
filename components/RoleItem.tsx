9708d416e7e4d1cac85d63ab27fe735b1cc25e83
Sam Downey <n00212512@iadt.ie>
2025-01-03 15:12:06 +0000

    updated styling for RoleItem component

--- components/RoleItem.tsx
+++ components/RoleItem.tsx
@@ -8,31 +8,48 @@ interface MyProps {
 
 export default function RoleItem({ role }: MyProps) {
     return (
-            <View style={styles.item}>
+        <View style={styles.card}>
+            <View style={styles.textContainer}>
+                {/* Role Title */}
                 <Link href={`/roles/${role._id}`}>
                     <Text style={styles.roleName}>{role.title}</Text>
                 </Link>
+
+                {/* Role Description */}
                 <Text style={styles.roleDesc}>{role.description}</Text>
             </View>
-        );
-};
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
-    item: {
-        backgroundColor: '#eaeaea',
-        padding: 20,
+    card: {
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "#FEF7FF", 
+        borderRadius: 12, 
+        padding: 16, 
         marginVertical: 8,
         marginHorizontal: 16,
-        borderRadius: 8,
+        shadowColor: "#000", 
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    textContainer: {
+        flex: 1,
+        marginRight: 16,
     },
     roleName: {
         fontSize: 18,
-        fontWeight: 'bold',
-        color: '#333',
+        fontWeight: "bold",
+        color: "#333",
+        marginBottom: 4,
     },
     roleDesc: {
         fontSize: 14,
-        color: '#555',
-        marginTop: 5,
-    }
+        color: "#333",
+        marginTop: 4,
+    },
 });
